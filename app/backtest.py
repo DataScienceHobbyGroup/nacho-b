9ea2bfe43c9e9ca59dc2a025e3c1c15526d1c5c1
Peter Ooms <20648110+peterooms@users.noreply.github.com>
2021-05-21 17:29:34 +0100

    Minor

--- app/backtest.py
+++ app/backtest.py
@@ -1,5 +1,6 @@
 import curio
 import logging
+logger = logging.getLogger(__name__)
 
 from datasources.base_class import datasource_base_class as ds
 from strategies.base_class import strategy_base_class as strat
@@ -8,6 +9,8 @@ from exchanges.base_class import exchange_base_class as exch
 class backtest_runner:
 
     async def run (strategy:strat, exchange:exch, datasource:ds, strategy_params:str, datasource_path:str):
+
+        logger.info("Entering backtest routine.")
 
         # Get curio queues
         transaction_queue = curio.Queue()
@@ -28,5 +31,8 @@ class backtest_runner:
             await g.cancel_remaining()
             async for task in g:
                 logging.info(str(task) + 'completed.' + str(task.result))
+
+        #Clean exit
+        logger.info("Backtest complete, exiting cleanly.")
 
     
