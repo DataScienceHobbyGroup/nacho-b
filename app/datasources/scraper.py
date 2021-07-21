fe834401c97fd68624d6e9e0d262c0452a918814
BarryFoye <barry.foye@gmail.com>
2021-07-21 22:34:26 +0100

    updated files to coding standards and added get_data and save_data functions

--- app/datasources/scraper.py
+++ app/datasources/scraper.py
@@ -7,20 +7,27 @@ from bs4 import BeautifulSoup
 
 
 class Scraper:
-    """TODO: Add classs description."""
+    """
+    This class is used to scrape data and information from websites.
+
+    It is a container for any bespoke logic related to a particular site
+    and the nuances for that site.
+
+    TODO: Currently this class is written with a bias to scraping a
+    specific class from a specific website. This can be improved to
+    handle multiple scenarios like multiple classes or other DOM elements.
+    """
 
     try:
         def scrape(website=None, _class=None):
-            """TODO: Add descriotion."""
+            """Scrape a website for a given class."""
             if not (website and _class) is None:
                 try:
                     page = requests.get(website)
                     soup = BeautifulSoup(page.content, 'html.parser')
                     _classes = soup.find_all(class_=_class)
                     tickers = [tick.text for tick in _classes]
-                    data = {'Tickers': tickers}
-                    info = pd.DataFrame(data, columns=['Tickers'])
-                    return tickers  # info['Tickers']
+                    return tickers
                 except Exception:
                     print('Failed to scrape')
             else:
--- app/datasources/ticker_scraper.py
+++ app/datasources/ticker_scraper.py
@@ -1,30 +1,50 @@
+"""
+Scraper for getting ticker data.
+
+Date: 2021
+Author: Barry Foye
+"""
+
 from app.datasources.scraper import scraper
 import yfinance as yf
 
+
 class TickerScraper:
-  YAHOO_FINANCE_TICKER_CLASS = 'Fw(600) C($linkColor)'
-  # read in site list
-  def read_in_site_list(self, file=None):
-    sites_urls = open(file)
-    list_of_sites = sites_urls.read().split('\n')
-    sites_urls.close()
-    return list_of_sites
+    """Collection of functions to gather financial data."""
 
-  # scrape tickers
-  def scrape_tickers(self, list_of_sites=None):
-    list_of_tickers = []
-    for s in list_of_sites:
-      list_of_tickers.extend(scraper.scrape(s, self.YAHOO_FINANCE_TICKER_CLASS))
-    return list_of_tickers
+    YAHOO_FINANCE_TICKER_CLASS = 'Fw(600) C($linkColor)'
 
-  # save scraped data
-  # ticker_data = []
-  # for i in range(4):
-  #   ticker_data.append(yf.Ticker(list_of_tickers[i]))
-  # for td in ticker_data:  
-  #   print(td.info)
+    def read_in_site_list(self, file=None):
+        """Read in site list."""
+        sites_urls = open(file)
+        list_of_sites = sites_urls.read().split('\n')
+        sites_urls.close()
+        return list_of_sites
+
+    def scrape_tickers(self, list_of_sites=None):
+        """Scrape ticker symbols."""
+        list_of_tickers = []
+        for s in list_of_sites:
+            list_of_tickers.extend(
+                scraper.scrape(s, self.YAHOO_FINANCE_TICKER_CLASS)
+            )
+        return list_of_tickers
+
+    def get_ticker_data(self, list_of_tickers=None):
+        """Get scraped ticker data."""
+        ticker_data = []
+        for i in range(4):
+            ticker_data.append(yf.Ticker(list_of_tickers[i]))
+
+    def save_ticker_data(self, data=None):
+        """Save data for later."""
+        for td in data:
+            print(td.info)
+            # TODO: for integration we need a way to store the scraped data
+            # in our DB.
+
 
 # Entrypoint
 if __name__ == '__main__':
-  ts = TickerScraper()
-  ts.read_in_site_list('resources/sites.txt')
+    ts = TickerScraper()
+    ts.read_in_site_list('resources/sites.txt')
